755e2b0bead0e02cc8642759b9671fa353a84100
mukuma <hallomamatha@gmail.com>
2021-12-19 09:20:23 -0500

    basic operations

---
+++ src/JavaTrail.java
@@ -0,0 +1,14 @@
+
+public class JavaTrail {
+//This a single line comment
+	/*
+	 * Multi line comment
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Have A Break, Have A Kit Kat");
+//		System.out.println("We Try Harder");
+//		System.out.println("Impossible is nothing");
+	}
+
+}

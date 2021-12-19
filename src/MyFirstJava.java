4d2babed5e1a0a4fc7030b911de0757b3aa23441
mukuma <hallomamatha@gmail.com>
2021-12-19 09:26:08 -0500

    Operations added

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -4,6 +4,12 @@ public class MyFirstJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mamatha");
+		int a=1;
+		int b=2;
+		System.out.println("Add:"+(a+b));
+		int x=3;
+		int y=4;
+		System.out.println("Multiply:"+(x*y));
 
 	}
 
